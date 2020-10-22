3148a3370af83c9376e46de8a0039e97d8838e21
Shubham Kumar <69601118+Shubham-Kumar-DTU@users.noreply.github.com>
2020-10-22 19:31:14 +0530

    Add files via upload

---
+++ Introductory problems/Weird Algo.cpp
@@ -0,0 +1,64 @@
+
+//GODLEON'S CODE
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ff                first
+#define ss                second
+#define int        	  long long
+#define ll                long long
+#define ddouble           long double  
+#define pb 		  push_back
+#define mp 		  make_pair
+#define pii               pair<int , int>
+#define vi                vector<int>
+#define mii               map<int,int>
+#define gsort             greater<int>
+#define pqb               priority_queue<int>
+#define pqs               priority_queue<int , vi , greater<int>>
+#define setbits(x)        _builtin_popcountll(x)
+#define zrobits(x)        _builtin_ctzll(x)
+#define mod               1000000007
+#define mod1              mod
+#define mod2              1000000009
+#define inf               1e18
+#define ps(x,y)           fixed<<setprecision(y)<<x
+#define mk(arr,n,type)    type *arr = new type[n] ;
+#define test(t)           int t ; cin >> t ; while(t--)
+#define ifor(a,b)         for(int i = a ; i < b ; i++)
+#define dfor(a,b)         for(int i = b-1 ; i >= a ; i--)
+#define fio               ios_base::sync_with_stdio(0) ; cin.tie(0) ; cout.tie(0) ;
+mt19937                   rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+// check odd even : n & 1
+// check power of 2 : n & n - 1 == 0
+//flip xor
+//set or
+//unset and
+//for -ve no.s mod ( a % mod + mod ) % mod ; 
+
+int32_t main()
+{
+int n;
+cin >> n;
+cout << n << " ";
+while(n != 1)
+{
+    if(n % 2 == 0)
+    {
+        n  =n / 2 ;
+        cout << n << " ";
+        continue;
+    }
+    else
+    {
+        n = n * 3 ;
+        n += 1;
+        cout << n  << " ";
+        continue;
+    }
+}
+}
